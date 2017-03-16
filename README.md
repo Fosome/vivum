1d24c98659c58096f8a48bc7b3fa56e26d0df6bc
Ryan Foster <ryan@mojotech.com>
2017-03-16 15:45:11 -0600

    Add basic seed data
    - create users and plants

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Vivum
 
 Succulents on Elixir.
+
+### Dependencies
+
+### Configuration
+
+### Testing
+
+### Seeds
+
+The application has seed data available for local development.  Run the seeds:
+
+```shell
+$ mix run priv/repo/seeds.exs
+```
+
+The seeds add several users you can authenticate as:
+
+```
+Username: alice
+Password: password
+```
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,80 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Vivum.Repo
+
+### Users
+
+user_data = [
+  %{
+    username:              "alice",
+    email:                 "alice@host.com",
+    password:              "password",
+    password_confirmation: "password"
+  },
+  %{
+    username:              "bob",
+    email:                 "bob@host.com",
+    password:              "password",
+    password_confirmation: "password"
+  },
+  %{
+    username:              "chuck",
+    email:                 "chuck@host.com",
+    password:              "password",
+    password_confirmation: "password"
+  },
+  %{
+    username:              "dale",
+    email:                 "dale@host.com",
+    password:              "password",
+    password_confirmation: "password"
+  },
+  %{
+    username:              "erin",
+    email:                 "erin@host.com",
+    password:              "password",
+    password_confirmation: "password"
+  },
+]
+
+Enum.each user_data, fn %{username: username} = params ->
+  Repo.get_by(Vivum.User, username: username) ||
+    %Vivum.User{}
+    |> Vivum.User.registration_changeset(params)
+    |> Repo.insert!()
+end
+
+alice = Repo.get_by(Vivum.User, username: "alice")
+
+
+### Plants
+
+plant_names = [
+  "African Milk Tree",
+  "Burro's Tail",
+  "Cactus Pear",
+  "Day Flower",
+  "Easter Cactus",
+  "Gold moss",
+  "Haworthia",
+  "Irish Ivy",
+  "Jade Tree",
+  "Kiwi Aeonium",
+  "Lace Aloe",
+  "Mothers Tongue",
+  "Naked Lady",
+  "Old Hens",
+  "Pencil Cactus",
+  "Ruby glow",
+  "Saguaro Cactus",
+  "Toad Cactus",
+  "Wax agave",
+  "Zebra plant"
+]
+
+Enum.each plant_names, fn name ->
+  Repo.get_by(Vivum.Plant, name: name, user_id: alice.id) ||
+    Repo.insert!(%Vivum.Plant{name: name, user_id: alice.id})
+end
